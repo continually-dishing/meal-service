bdf0f62e87801ebb465bd662909100441ea5bd6c
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-09-22 20:43:40 -0400

    Adding inital project

---
+++ Controllers/MealController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using meal_service.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace meal_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MealController : ControllerBase
+    {
+
+        private readonly ILogger<MealController> _logger;
+
+        public MealController(ILogger<MealController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetMeals()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public HttpResponse CreateMeal()
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public MealModel UpdateMeal()
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public HttpResponse DeleteMeal()
+        {
+            return null;
+        }
+    }
+}
---
+++ Models/MealModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace meal_service.Models
+{
+    public class MealModel
+    {
+        public String Name { get; set; }
+        public bool Vegetarian { get; set; }
+        public bool Vegan { get; set; }
+    }
+}
