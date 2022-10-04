58f25de7ffb285cf78d31c7a3f75b001bdabcad8
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-10-04 06:09:15 -0400

    Adding inital sql table

---
+++ .sql/meal_table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE meals (
+	id serial PRIMARY KEY,
+	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	prep_time TIME,
+	cook_time TIME,
+	vegan boolean NOT NULL,
+	vegetarian NOT NULL,
+	servings int,
+	ingridients text[] NOT NULL,
+);
