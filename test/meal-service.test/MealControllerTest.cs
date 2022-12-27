e0fb3fb01239428bc5263bb5cd51e7bcc63fcb06
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2022-12-26 22:59:27 -0500

    adding unit test for git meals

--- test/meal-service.test/MealControllerTest.cs
+++ test/meal-service.test/MealControllerTest.cs
@@ -1,31 +1,24 @@
-﻿using System;
-using AutoFixture;
+﻿using AutoFixture;
 using meal_service.Models;
-using meal_service.Repositories.Interfaces;
 using meal_service.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Moq;
 
-namespace meal_service.Test.Controllers
-{
-	public class MealControllerTest
-	{
+namespace meal_service.test;
+
+    public class MealControllerTest
+    {
         private readonly Mock<IMealService> _mockService;
         private readonly MealController _controller;
         private Fixture _fixture;
 
         public MealControllerTest()
-		{
+        {
             _fixture = new Fixture();
             _mockService = new Mock<IMealService>();
-            _controller= new MealController(_mockService.Object);
+            _controller = new MealController(_mockService.Object);
         }
 
-        public async Task CreateMeal_Success()
-        {
-           
-        }
 
         [Fact]
         public async Task GetMeals_Success()
@@ -35,7 +28,7 @@ namespace meal_service.Test.Controllers
             var response = await _controller.GetMeals();
             var obj = response as ObjectResult;
             Assert.Equal(200, obj.StatusCode);
+
         }
     }
-}
 
